089a0d44242db467900b49af550cf4e530ad7711
Peter Buckingham <peterbuckingham177@gmail.com>
2024-10-25 15:42:04 +0200

    Calculator Solution

--- JavaScript/javascript-exercises/08_calculator/calculator.js
+++ JavaScript/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) return 0;
+	return array.reduce((sum, num) => sum + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, num) => sum * num, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return (num**power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+      else {
+        return (num * factorial(num - 1));
+    }
 };
 
 // Do not edit below this line
