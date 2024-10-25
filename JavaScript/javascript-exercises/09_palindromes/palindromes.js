88cf757cf10eb0b9c2d14207cf31c0913a4416cf
Peter Buckingham <peterbuckingham177@gmail.com>
2024-10-25 15:59:15 +0200

    palindrome checker solution

--- JavaScript/javascript-exercises/09_palindromes/palindromes.js
+++ JavaScript/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const reversedSting = words.replace(/[\W_]/g, '').split('').reverse().join('').toLowerCase();
+    const cleanedString = words.replace(/[\W_]/g, '').toLowerCase()
+    return reversedSting === cleanedString
 };
 
 // Do not edit below this line
